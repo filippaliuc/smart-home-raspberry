8e621b9e441163e5db17ab12b2f795c07a9752b1
filippaliuc <paliucfilip@yahoo.com>
2023-06-21 16:56:42 +0300

    Added .gitignore

---
+++ firebaseConfig.py
@@ -0,0 +1,9 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyBlKwYLXbUPsK4IUQZ8_mhCBZeybiGh1kg",
+  "authDomain": "intelligent-home-project.firebaseapp.com",
+  "databaseURL": "https://intelligent-home-project-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "intelligent-home-project",
+  "storageBucket": "intelligent-home-project.appspot.com",
+  "messagingSenderId": "960213949958",
+  "appId": "1:960213949958:web:c6ad3c207e304aa1166d87"
+}
