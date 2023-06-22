6e59b74d4ded06bb44c1d1b783701a882284d610
Filip <paliucfilip@yahoo.com>
2023-06-22 13:25:03 +0300

    Issue resolved

--- main.py
+++ main.py
@@ -119,10 +119,10 @@ def feed_pet():
         prediction = database.child("predictie").child("tip").get()
         if prediction.val() == "Dog":
             print("Dog")
-	        time.sleep(10)
+            time.sleep(10)
         else:
             print("Cat")
-    	    time.sleep(10)
+            time.sleep(10)
 
 def cleanup():
     alarm_thread.join()
