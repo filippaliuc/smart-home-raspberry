c69cc17849214895c032d9f9ece39998de37d8e7
Filip <paliucfilip@yahoo.com>
2023-06-22 13:10:26 +0300

    Added more comments

--- tempAndHumidity.py
+++ tempAndHumidity.py
@@ -1,6 +1,8 @@
 import Adafruit_DHT
 
 def get_temperature_and_humidity():
-    humidity, temperature = Adafruit_DHT.read_retry(11,4)
+    # Citirea umidității și temperaturii utilizând modulul Adafruit_DHT
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
     return temperature, humidity
+
--- temperatureController.py
+++ temperatureController.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 import time
 
 def control_air_conditioner(temperature):
-
+    # Definirea pinului GPIO pentru controlul aerului condiționat
     INPUT_PIN = 32
 
+    # Configurarea modului GPIO și a pinului pentru controlul aerului condiționat
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(INPUT_PIN, GPIO.OUT)
 
+    # Controlul stării pinului GPIO în funcție de valoarea temperaturii
     GPIO.output(INPUT_PIN, temperature["clima"])
-        
-
