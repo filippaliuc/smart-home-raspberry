e8a2b9df16bb907523d12ba16cb5f3eaa00da27f
Filip <paliucfilip@yahoo.com>
2023-06-23 08:59:49 +0300

    Added

--- petFeederController.py
+++ petFeederController.py
@@ -11,15 +11,15 @@ def feed_cat():
     GPIO.setup(INPUT1_PIN, GPIO.OUT)
 
     # Controlul stării pinilor GPIO în funcție de predicție
-    GPIO.output(INPUT2_PIN, GPIO.HIGH)
+    GPIO.output(INPUT1_PIN, GPIO.HIGH)
 
     while True:
         prediction = database.child("predictie").child("tip").get()
         if prediction.val() == "Cat":
-            GPIO.output(INPUT2_PIN, GPIO.LOW)
+            GPIO.output(INPUT1_PIN, GPIO.LOW)
             print("Cat")
             sleep(10)
-            GPIO.output(INPUT2_PIN, GPIO.HIGH)
+            GPIO.output(INPUT1_PIN, GPIO.HIGH)
         sleep(30 * 60)
 
 def feed_dog():
@@ -31,15 +31,15 @@ def feed_dog():
     GPIO.setup(INPUT2_PIN, GPIO.OUT)
     
     # Controlul stării pinilor GPIO în funcție de predicție
-    GPIO.output(INPUT1_PIN, GPIO.HIGH)
+    GPIO.output(INPUT2_PIN, GPIO.HIGH)
 
     while True:
         prediction = database.child("predictie").child("tip").get()
         if prediction.val() == "Dog":
             print("Cat")
 
-            GPIO.output(INPUT1_PIN, GPIO.LOW)
+            GPIO.output(INPUT2_PIN, GPIO.LOW)
             sleep(10)
-            GPIO.output(INPUT1_PIN, GPIO.HIGH)
+            GPIO.output(INPUT2_PIN, GPIO.HIGH)
         sleep(30 * 60)
 
