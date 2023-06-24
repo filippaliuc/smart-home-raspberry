79170edd668e9d004ae9b81a60fc78e2f5af8e3a
Filip <paliucfilip@yahoo.com>
2023-06-24 20:25:16 +0300

    Test relay motor

--- humidityController.py
+++ humidityController.py
@@ -1,32 +1,16 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-# Pin definitions
-INPUT1_PIN = 16
-INPUT2_PIN = 18
-
-# GPIO setup and mode configuration
-def setup_gpio():
+def control_humidity(humidity):
+    # Definirea pinilor GPIO pentru umidificator și dezumidificator
+    INPUT1_PIN = 16
+    INPUT2_PIN = 18
+    
+    # Configurarea modului GPIO și a pinilor
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(INPUT1_PIN, GPIO.OUT)
     GPIO.setup(INPUT2_PIN, GPIO.OUT)
-
-# Control humidity based on the values of humidifier and dehumidifier
-def control_humidity(humidity, input1_pin, input2_pin):
-    GPIO.output(input1_pin, humidity[0])
-    GPIO.output(input2_pin, humidity[1])
-
-try:
-    # Setup GPIO pins
-    setup_gpio()
-
-    while True:
-        control_humidity([0, 1], INPUT1_PIN, INPUT2_PIN)
-        print([0, 1][0])
-        sleep(2)
-        control_humidity([1, 0], INPUT1_PIN, INPUT2_PIN)
-        print([1, 0][0])
-        sleep(2)
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    
+    # Controlul stării pinilor GPIO în funcție de valorile umidificatorului și dezumidificatorului
+    GPIO.output(INPUT1_PIN, humidity["umidificator"])
+    GPIO.output(INPUT2_PIN, humidity["dezumidificator"])
