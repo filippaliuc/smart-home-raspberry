6413b9a66dd6a6a0411eae7035cae22ad5920874
Filip <paliucfilip@yahoo.com>
2023-06-24 13:21:36 +0300

    configured blinds motor

--- blindsController.py
+++ blindsController.py
@@ -36,64 +36,26 @@ from time import sleep
 from guizero import App, Text, PushButton
 from gpiozero import Motor, LED
 
-ENABLE_PIN=21
-CLOCKWISE_PIN=23
-ANTI_CLOCKWISE_PIN=19
+ENABLE_PIN=9
+CLOCKWISE_PIN=10
+ANTI_CLOCKWISE_PIN=11
 
 motor = Motor(CLOCKWISE_PIN, ANTI_CLOCKWISE_PIN)
-motorSwitch = LED(27)
+motorSwitch = LED(ENABLE_PIN)
 
 app = App(title="GUI Development", layout="grid", height=600, width=800)
 message = Text(app, text="Single Motor Control Interface", grid=[4,0])
 
-motorSpeedForward = 0
-motorSpeedBackward = 0
+motorSpeedForward = 0.5
+motorSpeedBackward = 0.5
 
-def toggleSwitch():
-    if button0.text=="Start":
-       motorSwitch.on()
-       button0.text="Stop"
-    elif button0.text == "Stop":
-         motorSwitch.off()
-         button0.text = "Start"
 
-def forwardSpeedIncrease():
-    global motorSpeedForward
+def run():
+    motorSwitch.on()
     motor.forward(speed=motorSpeedForward)
-    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedForward))
-    motorSpeedForward += 0.1
-    if motorSpeedForward >= 1:
-        motorSpeedForward = 1
-
-def forwardSpeedReduce():
-    global motorSpeedForward
-    motor.forward(speed=motorSpeedForward)
-    print("Reduce speed of motor forward. Current speed = "+ str(motorSpeedForward))
-    motorSpeedForward -= 0.1
-    if motorSpeedForward <= 0:
-        motorSpeedForward = 0
-
-def backwardSpeedIncrease():
-    global motorSpeedBackward
-    motor.forward(speed=motorSpeedBackward)
-    print("Increased speed of motor backward. Current speed = "+ str(motorSpeedBackward))
-    motorSpeedBackward += 0.1
-    if motorSpeedBackward >= 1:
-        motorSpeedBackward = 1
-
-def backwardSpeedReduce():
-    global motorSpeedBackward
+    sleep(5)
     motor.backward(speed=motorSpeedBackward)
-    print("Reduce speed of motor backward. Current speed = "+ str(motorSpeedBackward))
-    motorSpeedBackward -= 0.1
-    if motorSpeedBackward <= 0:
-        motorSpeedBackward = 0
-
-Text(app, "Motor",grid=[2,1])
-button0 = PushButton(app, command=toggleSwitch, text="Start", width=10,height=3, grid=[2,4])
-button1 = PushButton(app, command=forwardSpeedIncrease, text="Frwd Speed +", width=10,height=3, grid=[2,3])
-button2 = PushButton(app, command=backwardSpeedReduce, text="Bckwd Speed -", width=10,height=3, grid=[2,5])
-button3 = PushButton(app, command=backwardSpeedIncrease, text = "Bckwd Speed +", width=10,height=3, grid=[1,4])
-button4 = PushButton(app, command=forwardSpeedReduce, text="Frwd Speed -", width=10, height=3, grid=[3,4])
-
-app.display()
+    sleep(5)
+    motorSwitch.off()
+    
+run()
