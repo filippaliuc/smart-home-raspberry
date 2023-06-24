91d989186c58aee613f74faf98e66a19d2388fbf
Filip <paliucfilip@yahoo.com>
2023-06-24 18:58:22 +0300

    Test relay motor

--- temperatureController.py
+++ temperatureController.py
@@ -4,7 +4,6 @@ import time
 def control_air_conditioner(state):
     # Definirea pinului GPIO pentru controlul aerului condiționat
     INPUT_PIN = 32
-    print(temperature)
     # Configurarea modului GPIO și a pinului pentru controlul aerului condiționat
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(INPUT_PIN, GPIO.OUT)
