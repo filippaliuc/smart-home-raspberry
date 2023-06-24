7382d5ff6334fc52d1fa529e213ad6c400d95dc5
Filip <paliucfilip@yahoo.com>
2023-06-24 13:55:23 +0300

    configured blinds motor

--- blindsController.py
+++ blindsController.py
@@ -32,25 +32,58 @@
 
 # blinds_controller(1)
 
+# from time import sleep
+# from gpiozero import Motor, LED
+
+# ENABLE_PIN=9
+# CLOCKWISE_PIN=10
+# ANTI_CLOCKWISE_PIN=11
+
+# motor = Motor(ANTI_CLOCKWISE_PIN, CLOCKWISE_PIN)
+# motorSwitch = LED(ENABLE_PIN)
+
+# motorSpeedForward = 1
+
+# def run():
+#     motorSwitch.on()
+#     sleep(2)
+#     motor.forward(speed=0.9)
+#     sleep(5)
+#     motor.backward(speed=0.9)
+#     sleep(5)
+#     motorSwitch.off()
+
+# run()
+
+import RPi.GPIO as GPIO
 from time import sleep
-from gpiozero import Motor, LED
 
-ENABLE_PIN=9
-CLOCKWISE_PIN=10
-ANTI_CLOCKWISE_PIN=11
+GPIO.setmode(GPIO.BOARD)
 
-motor = Motor(ANTI_CLOCKWISE_PIN, CLOCKWISE_PIN)
-motorSwitch = LED(ENABLE_PIN)
+ENABLE_PIN=21
+CLOCKWISE_PIN=19
+ANTI_CLOCKWISE_PIN=23
 
-motorSpeedForward = 1
+GPIO.setup(CLOCKWISE_PIN, GPIO.OUT)
+GPIO.setup(ANTI_CLOCKWISE_PIN, GPIO.OUT)
+GPIO.setup(ENABLE_PIN, GPIO.OUT)
+
 
 def run():
-    motorSwitch.on()
-    sleep(2)
-    motor.forward(speed=0.9)
-    sleep(5)
-    motor.backward(speed=0.9)
-    sleep(5)
-    motorSwitch.off()
+    pwm=GPIO.PWM(ENABLE_PIN, 100)
 
-run()
+    pwm.start(0)
+    GPIO.output(CLOCKWISE_PIN, GPIO.HIGH)
+    GPIO.output(ANTI_CLOCKWISE_PIN, GPIO.LOW)
+
+    pwm.ChangeDutyCycle(50)
+
+    GPIO.output(ENABLE_PIN, GPIO.HIGH)
+
+    sleep(2)
+
+    GPIO.output(ENABLE_PIN, GPIO.LOW)
+
+    pwm.stop()
+
+    GPIO.cleanup()
