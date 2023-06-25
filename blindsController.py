265a79da10b4bef035f3225ce8d9472af16ce788
Filip <paliucfilip@yahoo.com>
2023-06-25 16:58:49 +0300

    another try

--- blindsController.py
+++ blindsController.py
@@ -11,51 +11,51 @@ GPIO.setup(ENABLE_PIN, GPIO.OUT)
 GPIO.setup(MOTOR_PIN1, GPIO.OUT)
 GPIO.setup(MOTOR_PIN2, GPIO.OUT)
 
-pwm = GPIO.PWM(ENABLE_PIN, 100)
-try: 
-    while True:
-        pwm.start(0)
+# pwm = GPIO.PWM(ENABLE_PIN, 100)
+# try: 
+#     while True:
+#         pwm.start(0)
 
-        # Rotate clockwise
-        GPIO.output(MOTOR_PIN1, GPIO.HIGH)
-        GPIO.output(MOTOR_PIN2, GPIO.LOW)
+#         # Rotate clockwise
+#         GPIO.output(MOTOR_PIN1, GPIO.HIGH)
+#         GPIO.output(MOTOR_PIN2, GPIO.LOW)
 
-        for dc in range(0, 101, 5):
-            pwm.ChangeDutyCycle(dc)
-            time.sleep(0.1)
+#         for dc in range(0, 101, 5):
+#             pwm.ChangeDutyCycle(dc)
+#             time.sleep(0.1)
 
-        time.sleep(2)
+#         time.sleep(2)
 
-        # Rotate counterclockwise
-        GPIO.output(MOTOR_PIN1, GPIO.LOW)
-        GPIO.output(MOTOR_PIN2, GPIO.HIGH)
+#         # Rotate counterclockwise
+#         GPIO.output(MOTOR_PIN1, GPIO.LOW)
+#         GPIO.output(MOTOR_PIN2, GPIO.HIGH)
 
-        for dc in range(100, -1, -5):
-            pwm.ChangeDutyCycle(dc)
-            time.sleep(0.1)
+#         for dc in range(100, -1, -5):
+#             pwm.ChangeDutyCycle(dc)
+#             time.sleep(0.1)
 
-        time.sleep(2)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    pwm.stop()
-# print("FW")
+#         time.sleep(2)
+# except KeyboardInterrupt:
+#     GPIO.cleanup()
+#     pwm.stop()
+print("FW")
 
-# GPIO.output(ENABLE_PIN, GPIO.HIGH)
-# GPIO.output(MOTOR_PIN1, GPIO.HIGH)
-# GPIO.output(MOTOR_PIN2, GPIO.LOW)
+GPIO.output(ENABLE_PIN, GPIO.HIGH)
+GPIO.output(MOTOR_PIN1, GPIO.HIGH)
+GPIO.output(MOTOR_PIN2, GPIO.LOW)
 
-# time.sleep(4)
+time.sleep(4)
 
-# print("BW")
+print("BW")
 
-# GPIO.output(MOTOR_PIN2, GPIO.HIGH)
-# GPIO.output(MOTOR_PIN1, GPIO.LOW)
+GPIO.output(MOTOR_PIN2, GPIO.HIGH)
+GPIO.output(MOTOR_PIN1, GPIO.LOW)
 
 
-# time.sleep(4)
+time.sleep(4)
 
-# print("stop")
+print("stop")
 
-# GPIO.output(ENABLE_PIN, GPIO.LOW)
+GPIO.output(ENABLE_PIN, GPIO.LOW)
 
-# GPIO.cleanup()
+GPIO.cleanup()
