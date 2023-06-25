d27951ed6b3696b2fe879193f4ee6324558c0bd5
Filip <paliucfilip@yahoo.com>
2023-06-25 17:29:03 +0300

    another try

--- blindsController.py
+++ blindsController.py
@@ -13,35 +13,30 @@ def control_blinds(blinds_state, keep_track):
 
     pwm = GPIO.PWM(ENABLE_PIN, 100)
 
-    try:
+    pwm.start(0)
 
-        pwm.start(0)
-        while True:
-            if blinds_state and keep_track:
-                
-                keep_track = 0
+    if blinds_state and keep_track:
+            
+        keep_track = 0
 
-                # Coboară jaluzelele
-                GPIO.output(MOTOR_PIN1, GPIO.HIGH)
-                GPIO.output(MOTOR_PIN2, GPIO.LOW)
-                
+        # Coboară jaluzelele
+        GPIO.output(MOTOR_PIN1, GPIO.HIGH)
+        GPIO.output(MOTOR_PIN2, GPIO.LOW)
+        
 
-                pwm.ChangeDutyCycle(50)
+        pwm.ChangeDutyCycle(50)
 
-                time.sleep(2)
-            elif not blinds_state and not keep_track:
+        time.sleep(2)
+    elif not blinds_state and not keep_track:
 
-                keep_track = 1
+        keep_track = 1
 
-                # Ridică jaluzelele
-                GPIO.output(MOTOR_PIN1, GPIO.LOW)
-                GPIO.output(MOTOR_PIN2, GPIO.HIGH)
+        # Ridică jaluzelele
+        GPIO.output(MOTOR_PIN1, GPIO.LOW)
+        GPIO.output(MOTOR_PIN2, GPIO.HIGH)
 
-                for dc in range(100, -1, -5):
-                    pwm.ChangeDutyCycle(dc)
-                    time.sleep(0.1)
+        pwm.ChangeDutyCycle(50)
 
-                time.sleep(2)
+        time.sleep(2)
 
-    except KeyboardInterrupt:
-        pwm.stop()
+    pwm.stop()
