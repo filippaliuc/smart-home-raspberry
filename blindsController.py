4be3e1ade566307926376d48de86704d58f28428
Filip <paliucfilip@yahoo.com>
2023-06-29 21:53:27 +0300

    testing

--- blindsController.py
+++ blindsController.py
@@ -29,7 +29,7 @@ def control_blinds(blinds_state):
         GPIO.output(MOTOR_PIN2, GPIO.HIGH)
         
 
-        pwm.ChangeDutyCycle(70)
+        pwm.ChangeDutyCycle(65)
 
         time.sleep(1.5)
 
